4a5d5cf37dc4c6bed50585c1b0e9b99903d85bff
KTH1007 <taeheon@gimtaeheon-ui-MacBookPro.local>
2025-05-28 23:37:10 +0900

    Feat: page 객체를 DTO 형태로 변환하여 직렬화 #109

--- FreeMarket/src/main/java/com/freemarket/freemarket/FreeMarketApplication.java
+++ FreeMarket/src/main/java/com/freemarket/freemarket/FreeMarketApplication.java
@@ -8,9 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO) // 페이지네이션 JSON 구조 변경
 public class FreeMarketApplication {
 
     public static void main(String[] args) {
